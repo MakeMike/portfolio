79cdd1299a2b7506d653704753c22baaa04d690e
MakeMike <57689923+MakeMike@users.noreply.github.com>
2022-12-03 21:38:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggle(){
+    if (document.getElementById("toggle-menu").style.display === "none"){
+        document.getElementById("toggle-menu").style.display = "block";
+    }
+    else{
+        document.getElementById("toggle-menu").style.display = "none";
+    }
+}
